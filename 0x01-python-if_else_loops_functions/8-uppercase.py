657965a667431a319b8f89b4616cce4edca48a3f
AFOP <4592@holbertonschool.com>
2022-05-04 11:46:09 -0500

    Update Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) in range (97, 123):
+        if ord(c) in range(97, 123):
             resta = 32
         else:
             resta = 0
         print('{:c}'.format(ord(c) - resta), end="")
     print()
- 
