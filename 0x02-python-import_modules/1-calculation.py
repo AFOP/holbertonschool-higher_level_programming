7c2417aef1781d108766a0009fc9739051ce5b92
AFOP <4592@holbertonschool.com>
2022-05-05 17:55:05 -0500

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as f
+    a = 10
+    b = 5
+print(a, "+", b, "=", f.add(a, b))
+print(a, "-", b, "=", f.sub(a, b))
+print(a, "*", b, "=", f.mul(a, b))
+print(a, "/", b, "=", f.div(a, b))
