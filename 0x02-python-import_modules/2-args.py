9cf0feefc142b4f131a3ba45d69e78d0ad99325f
AFOP <4592@holbertonschool.com>
2022-05-05 18:57:43 -0500

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    print(len(argv) - 1, "argument", end="")
+    if len(argv) - 1 == 0:
+        print("s.")
+    else:
+        if len(argv) - 1 == 1:
+            print(":")
+        else:
+            print("s:")
+        for i in range(1,len(argv)):
+            print(i, argv[i])
