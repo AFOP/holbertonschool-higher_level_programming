11a6b3c5354f0326f57bcdde231d399bfaff66e7
AFOP <4592@holbertonschool.com>
2022-05-09 19:52:08 -0500

    Update Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,9 +4,9 @@ def new_in_list(my_list, idx, element):
         pass
     else:
         if idx <= 0:
-            return(my_list)
+            return(my_list.copy())
         if idx > len(my_list):
-            return(my_list)
+            return(my_list.copy())
         temp = my_list.copy()
         for i in range(len(temp)):
             if (i == idx):
