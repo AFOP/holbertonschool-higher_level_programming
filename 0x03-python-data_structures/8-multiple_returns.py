ae5db83cc7bf5e23ca6db0cb0ea5bae9e7abe183
AFOP <4592@holbertonschool.com>
2022-05-10 13:10:21 -0500

    Update Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        new_tuple = [len(sentence), none]
+        new_tuple = [0, none]
     else:
         new_tuple = [len(sentence), sentence[0]]
     return (new_tuple)
