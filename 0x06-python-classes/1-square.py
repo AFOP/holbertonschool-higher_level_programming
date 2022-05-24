cbb85a97610f215624ce739c7aa7b60c1a791fb0
AFOP <4592@holbertonschool.com>
2022-05-24 18:53:49 -0500

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" that defines a square by: (based on 0-square.py)"""
+
+
+class Square():
+    """ Class: Square """
+
+    def __init__(self, size):
+        """ arguments: size """
+
+        self._size = size
