babeaf34a7c3f0c48e5525c1e96be56a0d7dabd4
AFOP <4592@holbertonschool.com>
2022-06-07 18:44:59 -0500

    Task 1,2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns if the object is exactly an instance of the specified class """
+
+
+def is_same_class(obj, a_class):
+    """Return True or False in case Error"""
+
+    return type(obj) is a_class
