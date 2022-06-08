17211c0b01ddfcd41f96f8f9d2144dd4bec8e537
AFOP <4592@holbertonschool.com>
2022-06-08 16:30:27 -0500

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """method read_file"""
+
+    with open("my_file_0.txt") as f:
+        for line in f:
+            print(line, end="")
+        print()
+    f.close()
