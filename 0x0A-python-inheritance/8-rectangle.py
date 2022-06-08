2397fed581bffd90a1e1881116747f82b074e671
AFOP <4592@holbertonschool.com>
2022-06-08 11:46:34 -0500

    Update task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,10 +18,13 @@ class BaseGeometry:
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
+
     """class Rectangle inhered of BaseGeometry"""
 
     def __init__(self, width, height):
+
         """ method constructor """
 
         self.integer_validator("width", width)
