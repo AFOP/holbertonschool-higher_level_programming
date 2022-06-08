57c32e5e1bfd59dabab3d4ed60e6fff4b0963fe9
AFOP <4592@holbertonschool.com>
2022-06-07 23:59:12 -0500

    Task 8

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class BaseGeometry. """
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """method area"""
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """method area"""
+
+        
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class Rectangle """
+
+
+class Rectangle(BaseGeometry):
+    """that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """method init with width and height"""
