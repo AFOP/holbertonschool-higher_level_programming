ca08bd4ab5a3734fb146a05beea7da1b8eb96663
AFOP <4592@holbertonschool.com>
2022-06-07 23:07:57 -0500

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
 
     if type(obj) is a_class:
         return False
-    if isinstance(obj, a_class):
+    elif isinstance(obj, a_class):
         return True
     else:
         return False
