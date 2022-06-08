e9f5a2539f3f05713f0f191128d3a28ca4a7a42d
AFOP <4592@holbertonschool.com>
2022-06-08 15:01:54 -0500

    Update Task 8

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -38,4 +38,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """method magic str"""
 
-        return ("[{}] {}/{}".format(Rectangle.__name__, self.__width, self.__height))
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
