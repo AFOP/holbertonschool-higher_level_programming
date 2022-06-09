170059465c93bbdffcb979ef912098dcf1d2c022
AFOP <4592@holbertonschool.com>
2022-06-08 23:11:13 -0500

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,12 +4,11 @@ Python list, and then save them to a file"""
 
 import sys
 import os.path
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 lista = sys.argv[1:]
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 validator = os.path.exists(filename)
