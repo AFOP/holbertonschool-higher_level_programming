2dbb7f4d6fdbfa8799eb814a5b632eba740d9e0f
AFOP <4592@holbertonschool.com>
2022-06-09 11:54:24 -0500

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a class Student that defines a
+student by firts_name, last_name, age """
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dic = {}
+        dic = vars(self)
+        if attrs is None:
+            return self.__dict__
+        for name in dic:
+            for key in attrs:
+                if(name == key):
+                    new_dic[name] = dic.get(name)
+        return new_dic
