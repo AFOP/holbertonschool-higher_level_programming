dbc24f5f64bff67c2011b3e7ac3dc2b9063f729d
AFOP <4592@holbertonschool.com>
2022-06-09 22:36:52 -0500

    Update task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,43 +2,7 @@
 """ class BaseGeometry. """
 
 
-class BaseGeometry:
-    """class BaseGeometry"""
-
-    def area(self):
-        """method area"""
-
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """integer validator"""
-
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class Rectangle inhered of BaseGeometry"""
-
-    def __init__(self, width, height):
-        """ method constructor """
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """method area"""
-
-        return self.__width * self.__height
-
-    def __str__(self):
-        """method magic str"""
-
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -47,11 +11,15 @@ class Square(Rectangle):
     def __init__(self, size):
         """ method constructor """
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+
+    def __str__(self):
+        """method magic str"""
+
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """ method area """
 
-        return self.__size **2
+        return self.__size ** 2
