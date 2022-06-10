2ea5a9c00913dcc6628708d632223e43aca7cb54
AFOP <4592@holbertonschool.com>
2022-06-09 23:38:13 -0500

    Task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,15 +6,12 @@ representing the Pascal triangle of n """
 def pascal_triangle(n):
     """method pascal_triangle"""
 
-    new_list = []
-    i = 0
-    j = 1
+    lista = [[1],[1,1]]
 
-    for i in range(n):
-        new_list.append([[]])
-        new_list[i].append(1)
-        for j in range(1, i):
-            new_list[i].append(new_list[i-1][j-1] + new_list[i-1][j])
-        if i != 0:
-            new_list[i].append(1) 
-    return new_list
+    for i in range(1,n):
+        linea = [1]
+        for j in range(0,len(lista[i])-1):
+            linea.extend([ lista[i][j] + lista[i][j+1] ])
+        linea += [1]
+        lista.append(linea)
+    return lista
