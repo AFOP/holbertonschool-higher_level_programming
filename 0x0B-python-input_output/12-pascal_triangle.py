20632d604746da0987af10d4be494d90b86c2b51
AFOP <4592@holbertonschool.com>
2022-06-09 23:43:06 -0500

    Task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
 
     lista = [[1],[1,1]]
 
-    for i in range(1,n):
+    for i in range(1,n-1):
         linea = [1]
         for j in range(0,len(lista[i])-1):
             linea.extend([ lista[i][j] + lista[i][j+1] ])
