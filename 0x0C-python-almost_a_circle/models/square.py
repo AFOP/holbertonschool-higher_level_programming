56ed6101f3b935a3190e2eded884f396bd7a2df6
AFOP <4592@holbertonschool.com>
2022-06-13 22:42:33 -0500

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,9 +26,37 @@ class Square(Rectangle):
             raise ValueError("width must be >= 0")
         else:
             self.width = value
+            self.heigth = value
 
     def __str__(self):
         """method should return [Square] (<id>) 
         <x>/<y> - <size> - in our case, 
         width or height"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """*args is the list of arguments - no-keyworded arguments
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute"""
+        if args and args is not None: 
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]                    
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
