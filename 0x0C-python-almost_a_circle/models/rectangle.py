88ed00cd5eee5854090abcbdee4a0c1ce666c77f
AFOP <4592@holbertonschool.com>
2022-06-13 21:37:24 -0500

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,22 +102,34 @@ class Rectangle(Base):
         else: 
             return ("")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """that assigns an argument to each attribute:
         1st argument should be the id attribute
         2nd argument should be the width attribute
         3rd argument should be the height attribute
         4th argument should be the x attribute
         5th argument should be the y attribute"""
-        for i in range(len(args)):
-            if i == 0:
-                self.id = args[0]
-            if i == 1:
-                self.width = args[1]
-            if i == 2:
-                self.height = args[2]
-            if i == 3:
-                self.x = args[3]
-            if i == 4:
-                self.y = args[4]
-            
+        if args and args is not None: 
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
+        elif kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
