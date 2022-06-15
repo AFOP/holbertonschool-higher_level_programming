3d7fd3a15f22fa251871963a662ff6f4dfffda11
AFOP <4592@holbertonschool.com>
2022-06-15 15:20:57 -0500

    Update Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """method to_json_string"""
         return json.dumps(list_dictionaries)
