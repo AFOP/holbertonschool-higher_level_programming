bff378aafd457d44ec9d05723bc852d0944b1324
AFOP <4592@holbertonschool.com>
2022-07-13 22:09:22 -0500

    Update Task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,4 @@
 --  you need to convert all of the following to UTF8:
 --  Database hbtn_0c_0 Table first_table Field name in first_table
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name VARCHAR(191) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
