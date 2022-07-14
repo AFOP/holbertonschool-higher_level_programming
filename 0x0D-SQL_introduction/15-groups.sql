a3a245e0f9b69ad0b1b4d8434435da299caca77b
AFOP <4592@holbertonschool.com>
2022-07-13 21:32:21 -0500

    Update Task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 --  script that lists the number of records with the same score in the table second_table
 -- The result should display: the score, the number of records for this score with the label number
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY DESC;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
