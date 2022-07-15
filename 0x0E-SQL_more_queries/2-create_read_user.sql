98d1522f234e4ae545b3c0b3a3d90f731fc5f6bd
AFOP <4592@holbertonschool.com>
2022-07-14 22:28:58 -0500

    Update Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,4 @@
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXIST 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT PRIVILEGE ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
