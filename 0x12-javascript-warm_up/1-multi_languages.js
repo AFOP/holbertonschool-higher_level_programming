e350d583c19f488c8314cf7c1dab98dd18fd2a12
AFOP <4592@holbertonschool.com>
2022-08-16 11:29:43 -0500

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+Write a script that prints 3 lines:
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+const lineOne = 'C is fun';
+const lineTwo = 'Python is cool';
+const lineTree = 'JavaScript is amazing';
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineTree);
