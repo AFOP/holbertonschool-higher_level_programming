4f2f799edb5ec5625b250d16c16dfd257f848c00
AFOP <4592@holbertonschool.com>
2022-08-16 13:28:13 -0500

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+Write a script that prints the first argument passed to it:
+If no arguments are passed to the script, print “No argument”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use length
+*/
+const args = process.argv;
+let i = 0;
+while (args[i])
+{ 
+  if (i > 1) {
+    console.log(args[i]);
+  }
+  i++;
+}
+if (i === 2) {
+  console.log('No argument');
+}
