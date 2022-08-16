e343e2de4a35b7e336f643ba41ba467fe31bea94
AFOP <4592@holbertonschool.com>
2022-08-16 14:32:28 -0500

    Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Write a script that prints two arguments passed to it, in the following format: “ is ”
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+const args = process.argv;
+if (args.length > 3) {
+  console.log(args[2] + ' is ' + args[3]);
+} else if (args.length > 2) {
+  console.log(args[2] + ' is undefined');
+} else {
+  console.log('undefined is undefined');
+}
