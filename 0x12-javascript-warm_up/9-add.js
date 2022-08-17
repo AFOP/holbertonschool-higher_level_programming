fe877064aa67eb75523d9ff96583656d5224cdf3
AFOP <4592@holbertonschool.com>
2022-08-16 21:26:21 -0500

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+Write a script that prints the addition of 2 integers
+The first argument is the first integer
+The second argument is the second integer
+You have to define a function with this prototype: function add(a, b)
+You must use console.log(...) to print all output
+You are not allowed to use var
+*/
+const args = process.argv;
+let r = 0;
+if (args.length < 2) {
+  console.log('NaN');
+} else {
+  n1 = parseInt(args[2]);
+  n2 = parseInt(args[3]);
+  r = n1 + n2;
+  console.log(r);
+}
