02b12d7679ba469917f8ea21720d0494596554ab
AFOP <4592@holbertonschool.com>
2022-08-17 13:42:42 -0500

    Update task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,5 +4,5 @@ Write a function that returns the number of occurrences in a list:
 Prototype: exports.nbOccurences = function (list, searchElement)
 */
 exports.nbOccurences = function (array, value) {
-  return array.filter((cElm) => cElm === value).length;
-}
+  return (array.filter((cElm) => cElm === value).length);
+};
