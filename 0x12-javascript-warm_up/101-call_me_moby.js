8850512fcd9c742e9fdcdfaedfb248b8bad218f4
AFOP <4592@holbertonschool.com>
2022-08-17 20:51:13 -0500

    Task 101

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Write a function that executes x times a function.
+The function must be visible from outside
+Prototype: function (x, theFunction)
+You are not allowed to use var
+*/
+const Function = require('./101-main');
+exports.callMeMoby = function (x, theFunction) {
+  for (i = 0; i < x; i++) {
+    return (Function);
+  }
+};
