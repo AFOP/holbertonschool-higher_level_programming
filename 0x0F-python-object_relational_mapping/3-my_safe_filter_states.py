a6422f5ec5af115118c3dee466f1c527c1280109
AFOP <4592@holbertonschool.com>
2022-08-31 18:20:19 -0500

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Once again, write a script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!
+Your script should take 4 arguments: mysql username, mysql password,
+database name and state name searched (safe from MySQL injection)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = conn.cursor()
+    sql = "SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4])
+    cursor.execute(sql)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    conn.close()
