a1d3e76a11da537b7ce7ed460cc59e2f9b775c1f
AFOP <4592@holbertonschool.com>
2022-08-31 18:25:42 -0500

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Once again, write a script that takes in arguments and displays all values in
-the states table of hbtn_0e_0_usa where name matches the argument. But this time,
-write one that is safe from MySQL injections!
+the states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
 Your script should take 4 arguments: mysql username, mysql password,
 database name and state name searched (safe from MySQL injection)
 You must use the module MySQLdb (import MySQLdb)
@@ -19,8 +19,9 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s\
