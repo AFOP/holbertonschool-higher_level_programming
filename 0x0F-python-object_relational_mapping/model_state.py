c5fda719ab923dcc4eba054f6a52d9140c3b42f7
AFOP <4592@holbertonschool.com>
2022-08-31 22:16:52 -0500

    Task 5

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+from tkinter.tix import INTEGER
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State"""
+
+    __tablename__ = 'states'
+    id = Column(Integer(11), autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
