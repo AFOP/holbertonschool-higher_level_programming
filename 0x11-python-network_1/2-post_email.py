3cd2a1ead26755d2805074cc4ec2cf7722d48f50
AFOP <4592@holbertonschool.com>
2022-09-02 11:10:11 -0500

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+The email must be sent in the email variable
+You must use the packages urllib and sys
+You are not allowed to import packages other than urllib and sys
+You don't need to check arguments passed to the script (number or type)
+You must use the with statement
+Please test your script in the sandbox provided, using the web server running on port 5000
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
