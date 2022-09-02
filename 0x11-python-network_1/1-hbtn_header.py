f1682c801a8096669c253b697dc0dd577648c3fa
AFOP <4592@holbertonschool.com>
2022-09-02 09:56:20 -0500

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,10 +9,6 @@ The value of this variable is different for each request
 You don't need to check arguments passed to the script (number or type)
 You must use a with statement
 """
-#!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the value of the
-X-Request-Id variable found in the header of the response"""
-
 
 if __name__ == "__main__":
     import urllib.request
