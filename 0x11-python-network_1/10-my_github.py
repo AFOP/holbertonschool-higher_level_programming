9a6a40d9f375dde96da0ba6878696b157e4cabcd
AFOP <4592@holbertonschool.com>
2022-09-02 23:17:15 -0500

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches https://intranet.hbtn.io/status
-You must use the package requests
-You are not allow to import packages other than requests
-The body of the response must be display like the following
-example (tabulation before -)
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+You must use Basic Authentication with a personal access token
+as password to access to your information (only read:user permission is needed)
+The first argument will be your username
+The second argument will be your password
+(in your case, a personal access token as password)
+You must use the package requests and sys
+You are not allowed to import packages other than requests and sys
+You don't need to check arguments passed to the script (number or type)
 """
 if __name__ == "__main__":
     import requests
+    import sys
 
-    r = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    r = requests.get('https://swapi.co/api/people/?search={}'
+                     .format(sys.argv[1])).json()
+    print('Number of results: {}'.format(r.get('count')))
