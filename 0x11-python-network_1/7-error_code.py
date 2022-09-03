2e57be99200555d47d32873a8c75a2cd52f47ce5
AFOP <4592@holbertonschool.com>
2022-09-02 23:08:08 -0500

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches https://intranet.hbtn.io/status
-You must use the package requests
-You are not allow to import packages other than requests
-The body of the response must be display like the following
-example (tabulation before -)
+Write a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+If the HTTP status code is greater than or equal to 400, print:
+Error code: followed by the value of the HTTP status code
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don't need to check arguments passed to the script (number or type)
+Please test your script in the sandbox provided,
+using the web server running on port 5000
 """
+import requests
+from sys import argv
+
 if __name__ == "__main__":
-    import requests
-
-    r = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
