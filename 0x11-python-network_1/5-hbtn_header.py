f99aa2209c56c845caca74767ac5983e2c7cc3fa
AFOP <4592@holbertonschool.com>
2022-09-02 23:03:00 -0500

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that takes in a URL, 
+Write a Python script that takes in a URL,
 sends a request to the URL and displays the value of the variable
 X-Request-Id in the response header
 You must use the packages requests and sys
