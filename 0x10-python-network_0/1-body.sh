763e9a83c7bd50713a6bb775e2da4ba4075dde41
AFOP <4592@holbertonschool.com>
2022-09-28 20:11:10 -0500

    Update Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Display only body of a 200 status code response
-# You have to use curl
-# Please test your script in the sandbox provided, using the web server running on port 5000
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s "$1" -X GET -L
